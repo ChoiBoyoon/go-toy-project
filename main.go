39fdbfea8347a6f127c362f4873a05a4f9b1ec53
Boyoon Choi <chlqhdbs11@naver.com>
2022-01-30 00:03:44 +0100

    add extractJobs function

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 type extractedJob struct {
 	id string
@@ -22,4 +26,32 @@ func main() {
 	}
 
 	fmt.Println(jobs)
+}
+
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
+	
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("requesting: ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close() //prevent memory leaks
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErro(err)
+
+	searchCards := doc.Find(".tapItem")
+	searchCards.Each(func(i int, card *goquery.Selection){
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id,_ := card.Attr("data-jk")
+	title := cleanString(card.Find("h2>span").Text())
+	location := cleanString(card )
 }
